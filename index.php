f7930fa86aae5941fb1401c0b5e989ea8c3c1252
Leon van der Veen <vdvleon@gmail.com>
2015-08-05 14:58:38 +0200

    More details

--- index.php
+++ index.php
@@ -3,8 +3,9 @@ function safe($s) { return htmlspecialchars( (string) $s, ENT_QUOTES, 'utf-8', t
 function debug($var) { ob_start(); call_user_func_array('var_dump', func_get_args()); $html = '<pre>' . safe(ob_get_contents()) . '</pre>'; ob_end_clean(); return $html; }
 function arrGet(array $a, $k, $d = null) { return array_key_exists($k, $a) ? $a[$k] : $d; }
 ?>
-<h1>REMOTE</h1>
-<?= debug(arrGet($_SERVER, 'REMOTE_ADDR') . ':' . arrGet($_SERVER, 'REMOTE_PORT')) ?> 
+<h1>Details</h1>
+<?= debug('Remote: ' . arrGet($_SERVER, 'REMOTE_ADDR') . ':' . arrGet($_SERVER, 'REMOTE_PORT')) ?> 
+<?= debug('Environment: ' . arrGet($_SERVER, 'KOHANA_ENV')) ?> 
 <h1>$_SERVER</h1>
 <?= debug($_SERVER) ?> 
 <h1>$_GET</h1>
